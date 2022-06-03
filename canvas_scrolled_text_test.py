af6188c1675c6bfc55ea46366812a4af9830fa8f
Hatsu <>
2022-06-03 09:44:34 +0800

    Add background in test

--- canvas_scrolled_text_test.py
+++ canvas_scrolled_text_test.py
@@ -11,6 +11,22 @@ canvas = Canvas(root, width=CANVAS_SIZE[0], height=CANVAS_SIZE[1],
                 background="#ffffdd")
 canvas.pack()
 
+bg_gallery = [
+    PhotoImage(file="img/myLogo2_1.png"),
+    PhotoImage(file="img/myLogo2_2.png"),
+    PhotoImage(file="img/myLogo2_3.png"),
+]
+def get_bg_img():
+    return random.choice(bg_gallery)
+def get_bg_pos():
+    x = CANVAS_SIZE[0] / 2 + random.randrange(-CANVAS_SIZE[0] / 4, CANVAS_SIZE[0] / 4)
+    y = CANVAS_SIZE[1] / 2 + random.randrange(-CANVAS_SIZE[1] / 4, CANVAS_SIZE[1] / 4)
+    return (x, y)
+bg_x, bg_y = get_bg_pos()
+bg_id = canvas.create_image(bg_x, bg_y, image=get_bg_img(), anchor = CENTER)
+
+
+
 lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 article = " ".join([lorem] * 2)
 o = CanvasScrolledText(
@@ -34,16 +50,21 @@ def create_buttons():
             family=rand_font_name, size=rand_font_size, weight=rand_font_weight))
         o.update_debug_rect()
 
+    def random_bg():
+        bg_x, bg_y = get_bg_pos()
+        bg_img = get_bg_img()
+        canvas.itemconfigure(bg_id, image=bg_img)
+        canvas.coords(bg_id, bg_x, bg_y)
+
     button_frame = Frame(root)
     button_frame.pack(fill=X)
     buttons = [
         # 加按鈕加這裡即可
-        Button(button_frame, text="Add Text",
-               command=lambda: o.append_text(article)),
-        Button(button_frame, text="Reset Text",
-               command=lambda: o.set_text(article)),
+        Button(button_frame, text="Add Text", command=lambda: o.append_text(article)),
+        Button(button_frame, text="Reset Text", command=lambda: o.set_text(article)),
         Button(button_frame, text="Print Tag", command=lambda: print(o.tag)),
         Button(button_frame, text="Random Font", command=random_font),
+        Button(button_frame, text="Random BG", command=random_bg),
         Button(button_frame, text="Scroll (0, 0)", command=lambda: o.scroll(0, 0)),
         Button(button_frame, text="Scroll (10, 0)", command=lambda: o.scroll(10, 0)),
         Button(button_frame, text="Scroll (-10, 0)", command=lambda: o.scroll(-10, 0)),
