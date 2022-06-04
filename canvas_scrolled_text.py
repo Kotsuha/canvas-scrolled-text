251b7f59db9e7283d93837583577c441cd62cc60
Hatsu <>
2022-06-04 10:01:12 +0800

    Add progress bar

--- canvas_scrolled_text.py
+++ canvas_scrolled_text.py
@@ -1,5 +1,5 @@
 import uuid
-from tkinter import Canvas, Event
+from tkinter import HIDDEN, NORMAL, Canvas, Event
 from typing import Tuple, Union
 
 
@@ -57,6 +57,9 @@ class CanvasScrolledText:
         id_text = canvas.create_text(padx + 2, pady + 1, **options)
         # b = canvas.bbox(id_text)  # 你可以把 +2 +1 拿掉，用中斷點停在這行就知道意思了，會看到一個偏移 2px 一個偏移 1px (記得把 Window DPI Scaling 設成 100%)
 
+        # Create a vertical progress bar
+        id_line_y = canvas.create_line(canvas_size[0] - 2, 0, canvas_size[0] - 2, canvas_size[1], fill="#f0f0f0", width=4)
+
         # 創建 rect object (for debug)
         id_rect = canvas.create_rectangle(
             canvas.bbox(id_text), tags=(tag,), fill="gray")
@@ -73,6 +76,7 @@ class CanvasScrolledText:
         self.scrolly = scrolly
         self.tag = tag
         self.id_text = id_text
+        self.id_line_y = id_line_y
         self.id_rect = id_rect
         self.debug = False
 
@@ -81,6 +85,9 @@ class CanvasScrolledText:
         self.canvas.bind("<ButtonRelease-1>", self._on_release)
         self.canvas.bind("<MouseWheel>", self._on_mousewheel)
         self._last_drag = None
+
+        # Something can only be done until members vars set
+        self._update_progress_y()
 
     def get_text(self):
         return self.canvas.itemcget(self.id_text, "text")
@@ -94,6 +101,22 @@ class CanvasScrolledText:
         old_text = self.get_text()
         new_text = old_text + text
         self.set_text(new_text)
+
+    def _update_progress_y(self):
+        CAN_SIZE = self.canvas_size
+        bb = self.canvas.bbox(self.id_text)
+        h = bb[3] - bb[1]
+        ratio = CAN_SIZE[1] / h
+        if ratio >= 1:
+            self.canvas.itemconfigure(self.id_line_y, state=HIDDEN)
+            return
+        else:
+            self.canvas.itemconfigure(self.id_line_y, state=NORMAL)
+        x0 = CAN_SIZE[0] - 2
+        y0 = int((bb[1] * -1) * ratio)
+        x1 = x0
+        y1 = y0 + int(CAN_SIZE[1] * ratio)
+        self.canvas.coords(self.id_line_y, x0, y0, x1, y1)
 
     def scroll(self, x, y):
         txt_box = self.canvas.bbox(self.id_text)
@@ -134,6 +157,8 @@ class CanvasScrolledText:
         if self.debug:
             print(f"x: {x}")
         self.canvas.move(self.tag, x, y)
+        # Test
+        self._update_progress_y()
 
     def update_scroll(self):
         self.scroll(0, 0)
@@ -143,9 +168,9 @@ class CanvasScrolledText:
         """
         self.debug = value
         if value:
-            self.canvas.itemconfigure(self.id_rect, state="normal")
+            self.canvas.itemconfigure(self.id_rect, state=NORMAL)
         else:
-            self.canvas.itemconfigure(self.id_rect, state="hidden")
+            self.canvas.itemconfigure(self.id_rect, state=HIDDEN)
 
     def update_debug_rect(self):
         self.canvas.coords(self.id_rect, self.canvas.bbox(self.id_text))
